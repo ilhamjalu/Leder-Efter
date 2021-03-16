7880f7ca685ea6fa6e762fb22942ccfc8d9ca6d8
Farhan Muhammad <farhnmh@gmail.com>
2021-03-17 02:50:52 +0700

    Revert Old Version

---
+++ Leder Efter Server/Leder Efter Server/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using System.Collections.Generic;
+using ExternalMethods;
+
+namespace CoreProgram
+{
+    public class GeneralData
+    {
+        public static int clientOrder = 0;
+        public static string clientMessage = "";
+    }
+
+    public class ServerSide
+    {
+        static List<Database.Client> ClientDatabase = new List<Database.Client>();
+        static int playerId;
+
+        public static void ClientProcess(object argument)
+        {
+            TcpClient client = (TcpClient)argument;
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+            NetworkStream ns = client.GetStream();
+            //Data DataClient = new Data();
+
+            ExternalMethods.LoginPage loginPage = new LoginPage();
+            ExternalMethods.ChatBox chatBox = new ChatBox();
+
+            try
+            {
+                while (true)
+                {
+                    string process = reader.ReadLine();
+
+                    if (process == "0")
+                    {
+                        loginPage.SignIn(ns, writer, ClientDatabase);
+                        playerId = loginPage.getterId();
+                    }
+                    else if (process == "1")
+                    {
+                        loginPage.SignUp(ns, writer, ClientDatabase);
+                    }
+                    else if (process == "3")
+                    {
+                        ClientDatabase[playerId].message = chatBox.ReceiveClientMessage(reader);
+                        Console.WriteLine(" " + ClientDatabase[playerId].username + ": " + ClientDatabase[playerId].message);
+                        chatBox.BroadcastClientMessage(writer, ClientDatabase[playerId].username + ": " + ClientDatabase[playerId].message);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("\n [has come out]");
+            }
+        }
+
+        public static void Main()
+        {
+            TcpListener listener = null;
+
+            try
+            {
+                ClientDatabase.Add(new Database.Client());
+                ClientDatabase[0].setterData(ClientDatabase.Count - 1, "admin", "admin");
+
+                Console.WriteLine(" [SERVER-SIDE]");
+                Console.Write(" [Input Your IP Address]: ");
+                string ipAddress = Console.ReadLine();
+
+                listener = new TcpListener(IPAddress.Parse(ipAddress), 8080);
+                listener.Start();
+
+                Console.WriteLine(" [Starting Program...]");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine(" [There's Client Join...]");
+                    Thread newThread = new Thread(ClientProcess);
+
+                    GeneralData.clientOrder++;
+                    newThread.Start(client);
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            finally
+            {
+                if (listener != null)
+                {
+                    listener.Stop();
+                }
+            }
+        }
+    }
+
+    sealed class CustomizedBinder : SerializationBinder
+    {
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            Type returntype = null;
+            string sharedAssemblyName = "SharedAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+            assemblyName = Assembly.GetExecutingAssembly().FullName;
+            typeName = typeName.Replace(sharedAssemblyName, assemblyName);
+            returntype =
+                    Type.GetType(String.Format("{0}, {1}",
+                    typeName, assemblyName));
+
+            return returntype;
+        }
+
+        public override void BindToName(Type serializedType, out string assemblyName, out string typeName)
+        {
+            base.BindToName(serializedType, out assemblyName, out typeName);
+            assemblyName = "SharedAssembly, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+        }
+    }
+}
